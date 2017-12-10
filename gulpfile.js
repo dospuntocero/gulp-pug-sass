f25f2ecb11ad1a7e07758f22b2bcd98c05f82328
Francisco arenas <fa@dospuntocero.cl>
2017-12-10 00:16:16 -0300

    Update gulpfile.js
    better organization and streaming to the browser

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,42 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var pug = require('gulp-pug');
-var browserSync = require('browser-sync').create();
-
-const autoprefixer = require('gulp-autoprefixer');
+var gulp        	= require('gulp');
+var sass        	= require('gulp-sass');
+var pug        		= require('gulp-pug');
+var browserSync 	= require('browser-sync').create();
+const autoprefixer 	= require('gulp-autoprefixer');
 
 //sass
 gulp.task('sass', function(){
-    return gulp.src('./src/scss/main.scss')
-        .pipe(sass({ style: 'compressed' }))
-        .pipe(autoprefixer('last 10 version'))
-        .pipe(gulp.dest('./dist/css'));
+	return gulp.src('./src/scss/main.scss')
+		.pipe(sass({ style: 'compressed' }))
+		.pipe(autoprefixer('last 10 version'))
+		.pipe(gulp.dest('./dist/css'))
+		.pipe(browserSync.reload({
+			stream: true
+		}));
 });
 
 //pug
-gulp.task('pug', function () {
+gulp.task('pug',function () {
 	var database = require('./src/pug/db/database.json');
-    return gulp.src('./src/pug/**.pug')
-        .pipe(pug({'pretty':true,'locals':database}))
-        .pipe(gulp.dest('./dist'));
+	return gulp.src('./src/pug/**/*.pug')
+		.pipe(pug({'pretty':true,'locals':database}))
+		.pipe(gulp.dest('./dist'))
+		.pipe(browserSync.reload({
+			stream: true
+		}));
 });
 
 //server
 gulp.task('serve', ['sass'], function() {
-    browserSync.init({
-        server: "./dist"
-    });
-    gulp.watch("./src/scss/*.scss", ['sass']).on('change', browserSync.reload);
-    gulp.watch('./src/pug/**', ['pug']).on('change', browserSync.reload);
-    gulp.watch('./src/pug/db/*.json', ['pug']).on('change', browserSync.reload);
+	browserSync.init({
+		server: "./dist"
+	});
 });
 
 // Default task
-gulp.task('default', function () {
-    gulp.start('sass');
-    gulp.start('pug');    
-    gulp.start('serve');
+gulp.task('default', ['sass', 'pug','serve'], function () {
+	gulp.watch("./src/scss/**/*.scss", ['sass']);
+	gulp.watch('./src/pug/**/*.pug', ['pug']);
+	gulp.watch('./src/pug/db/*.json', ['pug']);
 });
+
