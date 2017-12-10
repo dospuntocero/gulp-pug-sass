c94709032c4e56a28bd0fc7ebde48cc83a3a6070
Francisco arenas <fa@dospuntocero.cl>
2017-12-10 13:39:09 -0300

    Update gulpfile.js
    adding gulp-plumber to avoid pipe breaking when something is not right in sass files

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,19 @@
-var gulp        	= require('gulp');
-var sass        	= require('gulp-sass');
-var pug        		= require('gulp-pug');
-var browserSync 	= require('browser-sync').create();
-const autoprefixer 	= require('gulp-autoprefixer');
+var gulp        = require('gulp');
+var sass        = require('gulp-sass');
+var pug         = require('gulp-pug');
+var browserSync = require('browser-sync').create();
+var plumber     = require('gulp-plumber');
+
+const autoprefixer = require('gulp-autoprefixer');
 
 //sass
 gulp.task('sass', function(){
 	return gulp.src('./src/scss/main.scss')
+		.pipe(plumber())
 		.pipe(sass({ style: 'compressed' }))
 		.pipe(autoprefixer('last 10 version'))
 		.pipe(gulp.dest('./dist/css'))
-		.pipe(browserSync.reload({
-			stream: true
-		}));
+		.pipe(browserSync.reload({stream: true}));
 });
 
 //pug
@@ -21,22 +22,27 @@ gulp.task('pug',function () {
 	return gulp.src('./src/pug/**/*.pug')
 		.pipe(pug({'pretty':true,'locals':database}))
 		.pipe(gulp.dest('./dist'))
-		.pipe(browserSync.reload({
-			stream: true
-		}));
+		.pipe(browserSync.reload({stream: true}));
+});
+
+//copy images to the dist folder
+gulp.task('images',function () {
+	gulp.src('./src/images/**.*')
+		.pipe(gulp.dest('./dist/images'));
 });
 
 //server
 gulp.task('serve', ['sass'], function() {
 	browserSync.init({
-		server: "./dist"
-	});
+		                 server: "./dist"
+	                 });
 });
 
+
 // Default task
-gulp.task('default', ['sass', 'pug','serve'], function () {
-	gulp.watch("./src/scss/**/*.scss", ['sass']);
+gulp.task('default', ['sass', 'pug','images','serve'], function () {
+	gulp.watch("./src/images/**.*", ['images']);
+	gulp.watch("./src/scss/**/**/*.scss", ['sass']);
 	gulp.watch('./src/pug/**/*.pug', ['pug']);
 	gulp.watch('./src/pug/db/*.json', ['pug']);
 });
-
