ea1d32bd0127fb36e7444733deb223e97739b22e
Francisco arenas <fa@dospuntocero.cl>
2019-06-03 07:54:29 -0400

    Update gulpfile.js
    now the server wont die when you fail writting a pug file. it will wait until you fix it for compile again

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp        = require('gulp');
 var sass        = require('gulp-sass');
+var sourcemaps  = require('gulp-sourcemaps');
 var pug         = require('gulp-pug');
 var browserSync = require('browser-sync').create();
 var plumber     = require('gulp-plumber');
@@ -9,9 +10,11 @@ const autoprefixer = require('gulp-autoprefixer');
 //sass
 gulp.task('sass', function(){
 	return gulp.src('./src/scss/main.scss')
+		.pipe(sourcemaps.init())
 		.pipe(plumber())
 		.pipe(sass({ style: 'compressed' }))
 		.pipe(autoprefixer('last 10 version'))
+		.pipe(sourcemaps.write('.'))
 		.pipe(gulp.dest('./dist/css'))
 		.pipe(browserSync.reload({stream: true}));
 });
@@ -20,6 +23,7 @@ gulp.task('sass', function(){
 gulp.task('pug',function () {
 	var database = require('./src/pug/db/database.json');
 	return gulp.src('./src/pug/**/*.pug')
+		.pipe(plumber())
 		.pipe(pug({'pretty':true,'locals':database}))
 		.pipe(gulp.dest('./dist'))
 		.pipe(browserSync.reload({stream: true}));
@@ -28,19 +32,26 @@ gulp.task('pug',function () {
 //copy images to the dist folder
 gulp.task('images',function () {
 	gulp.src('./src/images/**.*')
-		.pipe(gulp.dest('./dist/images'));
+		.pipe(gulp.dest('./dist/images'))
+		.pipe(browserSync.reload({stream: true}));
 });
 
+gulp.task('copy', function () {
+	gulp.src('./src/js/**.*')
+		.pipe(gulp.dest('./dist/js'))
+		.pipe(browserSync.reload({stream: true}));
+});
 //server
 gulp.task('serve', ['sass'], function() {
 	browserSync.init({
-		                 server: "./dist"
-	                 });
+   server: "./dist"
+ });
 });
 
 
 // Default task
-gulp.task('default', ['sass', 'pug','images','serve'], function () {
+gulp.task('default', ['sass', 'pug','images','copy','serve'], function () {
+	gulp.watch("./src/js/**.*", ['copy']);
 	gulp.watch("./src/images/**.*", ['images']);
 	gulp.watch("./src/scss/**/**/*.scss", ['sass']);
 	gulp.watch('./src/pug/**/*.pug', ['pug']);
